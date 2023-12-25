bb03686f75cb253e83d81ed1125a796b96b77118
PiriyaLapa <piriyalapa@gmail.com>
2023-12-25 13:30:57 +0700

    Added Navbar and try deploy

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,16 +1,24 @@
-import {Component, ReactNode} from 'react';
-import styled from 'styled-components';
-const StyledNavbar = styled.nav`
-    background-color:green;
-`; 
+import { Component, ReactNode } from "react";
+import styled from "styled-components";
+import { theme } from "../utility";
 
-export default class Navbar extends Component{
-    render():ReactNode{
-        return(
-            <StyledNavbar>
-                <h1>Logo</h1>
-                <button type="submit">buton</button>
-            </StyledNavbar>            
-        )
-    }
+const StyledNavbar = styled.nav<{ theme: typeof theme }>`
+  width:90%;
+  background-color: ${(props) => props.theme.darkPurpleColor};
+  border: 1px solid ${(props) => props.theme.EncaplyptusColor};
+  display: flex;
+  margin:0 auto;
+  justify-content:space-between;
+  padding: 10px;
+`;
+
+export default class Navbar extends Component {
+  render(): ReactNode {
+    return (
+      <StyledNavbar theme={theme}>
+        <h1>Logo</h1>
+        <button type="submit">buton</button>
+      </StyledNavbar>
+    );
+  }
 }

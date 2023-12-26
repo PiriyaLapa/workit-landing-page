32fd47cee217e8d92594bbd7c7a540aa18efd6fe
PiriyaLapa <piriyalapa@gmail.com>
2023-12-26 11:14:08 +0700

    update

--- src/Components/ButtonComponents.tsx
+++ src/Components/ButtonComponents.tsx
@@ -1,16 +1,21 @@
 import { Component, ReactNode } from 'react';
 import styled from 'styled-components';
+import {theme} from '../utility';
 
-const StylePrimaryBtn = styled.button`
+const StylePrimaryBtn = styled.button<{theme: typeof theme}>`
     // width:11.875em;
     height:3.813em;
     width:15%;
+    background-color:${(props) => props.theme.EncaplyptusColor};
+    text-transform:capitalize;
+    font-size:bold;
+
 `;
 
 export class PrimaryBtn extends Component{
     render():ReactNode{
         return(
-            <StylePrimaryBtn>apple for access</StylePrimaryBtn>
+            <StylePrimaryBtn theme={theme}>apple for access</StylePrimaryBtn>
         )
     }
 }

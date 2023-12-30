982fbc041fe334fc8ee881d233b584909a525e11
PiriyaLapa <piriyalapa@gmail.com>
2023-12-30 21:45:39 +0700

    update and try to deploy

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,10 +2,10 @@ import { Component, ReactNode } from "react";
 import styled from "styled-components";
 import { theme } from "../utility";
 import Navbar from "./Navbar";
-// import ImgContainer from "../assets/Rectangle.png";
-// import HeroImgSource from "../assets/moblie.png";
-// import { HeroImg } from "./ImgHero";
-// import { PrimaryBtn } from "./ButtonComponents";
+import ImgContainer from "../assets/Rectangle.png";
+import HeroImgSource from "../assets/moblie.png";
+import { HeroImg } from "./ImgHero";
+import { PrimaryBtn } from "./ButtonComponents";
 
 const HeaderContainer = styled.div<{
   theme: typeof theme;
@@ -17,38 +17,38 @@ const HeaderContainer = styled.div<{
   background-color: ${(props) => props.theme.darkPurpleColor};
 `;
 
-// const InnerBox = styled.div`
-//   display: block;
-//   align-items: center;
-//   * {
-//     margin: 38px auto;
-//   }
-// `;
+const InnerBox = styled.div`
+  display: block;
+  align-items: center;
+  * {
+    margin: 38px auto;
+  }
+`;
 
-// const TitleHeader = styled.span`
-//   width: 40%;
-//   height: auto;
-//   font-family: "Fraunces 144pt";
-//   font-size: 80px;
-//   font-weight: 100;
-//   line-height: 80px;
-//   text-align: center;
-//   display: block;
-//   margin-top: 50px;
-//   color: white;
-//   margin: 50px auto;
-// `;
+const TitleHeader = styled.span`
+  width: 40%;
+  height: auto;
+  font-family: "Fraunces 144pt";
+  font-size: 80px;
+  font-weight: 100;
+  line-height: 80px;
+  text-align: center;
+  display: block;
+  margin-top: 50px;
+  color: white;
+  margin: 50px auto;
+`;
 
 export default class Header extends Component {
   render(): ReactNode {
     return (
         <HeaderContainer theme={theme}>
           <Navbar />
-          {/* <InnerBox>
+          <InnerBox>
             <TitleHeader>Data tailored to your deep.</TitleHeader>
             <PrimaryBtn />
           <HeroImg urlImg={HeroImgSource} />
-          </InnerBox> */}
+          </InnerBox>
         </HeaderContainer>
     );
   }

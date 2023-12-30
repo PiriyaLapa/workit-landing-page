fa28b2446713c951a15d71c145b23fce8e333165
Piriya <piriyalapa@gmail.com>
2023-12-30 11:38:59 +0700

    update card

---
+++ src/Components/CardContent.tsx
@@ -0,0 +1,16 @@
+import {Component,ReactNode} from 'react';
+import styled from 'styled-components';
+
+interface ContentProps{
+
+}
+
+export default class CardContent Component<ContentProps>{
+    render():ReactNode{
+        return(
+            <StyleCard>
+
+            </StyleCard>
+        )
+    }
+}

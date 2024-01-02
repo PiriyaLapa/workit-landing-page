e7d4b6066e3607a022955afffd424fbce0f02a2e
PiriyaLapa <piriyalapa@gmail.com>
2024-01-02 22:49:39 +0700

    update and try to deploy

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -8,27 +8,24 @@ const CircleShape = styled.div<{ shape: typeof shape; fonts: typeof fonts }>`
   height: 36px;
   border-radius: 50%;
   border: 1px solid ${(props) => props.theme.darkPurpleColor};
-  font-family:${(props) => props.fonts.PrimaryFont};
-  font-size:1.2em;
-  font-style:normal;
-  font-weigth:600;
-  color:${(props) => props.theme.darkPurpleColor};
-  text-align:center;
-  margin:5px auto;
+  font-family: ${(props) => props.fonts.PrimaryFont};
+  font-size: 1.2em;
+  font-style: normal;
+  font-weigth: 600;
+  color: ${(props) => props.theme.darkPurpleColor};
+  text-align: center;
+  margin: 5px auto;
 `;
 
-const TitleCard = styled.h1<{shape:typeof shape, fonts: typeof fonts}>`
-  
-`;
+const TitleCard = styled.h1<{ shape: typeof shape; fonts: typeof fonts }>``;
 
 const CardContainer = styled.div`
-  display:flex;
+  display: flex;
 `;
 
 // const ContentCard = styled.p<{}>`
 
 // `;
-
 
 interface CardProps {
   Data: CARDData[];
@@ -44,7 +41,9 @@ export default class Card extends Component<CardProps> {
             <CircleShape shape={shape} fonts={fonts}>
               <h2>{data.id}</h2>
             </CircleShape>
-            <TitleCard shape={shape} fonts={fonts}>{data.title}</TitleCard>
+            <TitleCard shape={shape} fonts={fonts}>
+              {data.title}
+            </TitleCard>
             <p>{data.content}</p>
           </div>
         ))}

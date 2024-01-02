058f27756694c769a18ab575f7cae708a3e5d1e2
PiriyaLapa <piriyalapa@gmail.com>
2024-01-02 12:02:31 +0700

    updpate and try to deloy

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,14 +1,34 @@
 import { Component, ReactNode } from "react";
 import { CARDData } from "../Data/Data";
 import styled from "styled-components";
-import { shape } from "../utility";
+import { shape, fonts } from "../utility";
 
-const CircleShape = styled.div<{ shape: typeof shape }>`
+const CircleShape = styled.div<{ shape: typeof shape; fonts: typeof fonts }>`
   width: 36px;
   height: 36px;
-  border-radius:50%;
-  border:1px solid red;
+  border-radius: 50%;
+  border: 1px solid ${(props) => props.theme.darkPurpleColor};
+  font-family:${(props) => props.fonts.PrimaryFont};
+  font-size:1.2em;
+  font-style:normal;
+  font-weigth:600;
+  color:${(props) => props.theme.darkPurpleColor};
+  text-align:center;
+  margin:5px auto;
 `;
+
+const TitleCard = styled.h1<{shape:typeof shape, fonts: typeof fonts}>`
+  
+`;
+
+const CardContainer = styled.div`
+  display:flex;
+`;
+
+// const ContentCard = styled.p<{}>`
+
+// `;
+
 
 interface CardProps {
   Data: CARDData[];
@@ -18,15 +38,17 @@ export default class Card extends Component<CardProps> {
   render(): ReactNode {
     const { Data } = this.props;
     return (
-      <div>
+      <CardContainer>
         {Data.map((data, index) => (
           <div key={index}>
-            <CircleShape shape={shape}><h2>{data.id}</h2></CircleShape>
-            <h2>{data.title}</h2>
+            <CircleShape shape={shape} fonts={fonts}>
+              <h2>{data.id}</h2>
+            </CircleShape>
+            <TitleCard shape={shape} fonts={fonts}>{data.title}</TitleCard>
             <p>{data.content}</p>
           </div>
         ))}
-      </div>
+      </CardContainer>
     );
   }
 }
--- src/Components/MiddleSection.tsx
+++ src/Components/MiddleSection.tsx
@@ -21,6 +21,7 @@ const MiddleInnerBox = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
 `;
 
 export default class MiddleSection extends Component {

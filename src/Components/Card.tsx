34447f5977159fea48582ee3d93b697d1b292e6f
Piriya <piriyalapa@gmail.com>
2024-01-04 00:23:52 +0700

    try to change middle section and deploy it

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -4,17 +4,17 @@ import styled from "styled-components";
 import { shape, fonts, theme } from "../utility";
 
 const CircleShape = styled.div<{ shape: typeof shape; fonts: typeof fonts }>`
-  width: 36px;
-  height: 36px;
+  width: 42px;
+  height: 42px;
   border-radius: 50%;
   border: 1px solid ${(props) => props.theme.darkPurpleColor};
   font-family: ${(props) => props.fonts.PrimaryFont};
   font-size: 1.2em;
   font-style: normal;
-  font-weight:600;
+  font-weight: 600;
   color: ${(props) => props.theme.darkPurpleColor};
   text-align: center;
-  margin: 80px auto;
+  margin: 45px auto;
 `;
 
 const TitleCard = styled.h1<{ shape: typeof shape; fonts: typeof fonts }>`
@@ -23,16 +23,18 @@ const TitleCard = styled.h1<{ shape: typeof shape; fonts: typeof fonts }>`
 
 const CardContainer = styled.div`
   display: flex;
-  justify-content:space-between;
+  justify-content: space-between;
 `;
 
-const ContentCard = styled.p<{fonts:typeof fonts}>`
+const ContentCard = styled.p<{ fonts: typeof fonts }>`
   font-family: ${(props) => props.fonts.SecondaryFont};
   margin: 20px 13px;
 `;
 
-const CardWrap = styled.div<{theme:typeof theme}>`
-  color:${(props) => props.theme.darkPurpleColor};
+const CardWrap = styled.div<{ theme: typeof theme }>`
+  color: ${(props) => props.theme.darkPurpleColor};
+  border:1px solid red;
+  position:relative;
 `;
 
 interface CardProps {
--- src/Components/MiddleSection.tsx
+++ src/Components/MiddleSection.tsx
@@ -9,10 +9,10 @@ import ImgMiddleContainer from '../assets/MiddleContainer.png';
 const MiddleContainer = styled.div<{ theme: typeof theme; urlImg: {ImgMiddleContainer:string}}>`
   /* width: 1440px; */
   width:100%;
-  height: 730px;
+  height: 1000px;
   margin: 0 auto;
   /* clip-path: ${(props) => props.theme.HeaderShape}; */
-  background-color: ${(props) => props.theme.GhostWhiteColor};
+  /* background-color: ${(props) => props.theme.GhostWhiteColor}; */
   background-image: url(${(props) => props.urlImg.ImgMiddleContainer});
   background-position:center;
   background-repeat:no-repeat;
@@ -20,6 +20,7 @@ const MiddleContainer = styled.div<{ theme: typeof theme; urlImg: {ImgMiddleCont
   position: relative;
   top: -70px;
   z-index: -1;
+  
 `;
 const MiddleInnerBox = styled.div`
   width:1114px;
